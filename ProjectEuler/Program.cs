b20120cbf68a4325fe1e0bff141e21b4cc3223cd
John Gietzen <otac0n@gietzen.us>
2010-05-29 15:58:53 +0000

    Added solution to Problem 40.
    git-svn-id: http://otac0n.com:3000/projects/ProjectEuler/trunk@1472 14c0b8bc-c9cf-8f41-ba33-e04da095c157

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -34,7 +34,7 @@ namespace ProjectEuler
             var sw = new Stopwatch();
             sw.Start();
 
-                Problem_089();
+                Problem_040();
 
             sw.Stop();
             Console.WriteLine();
@@ -2074,6 +2074,35 @@ namespace ProjectEuler
             }
 
             Console.WriteLine("sum = " + sum);
+        }
+
+        /// <summary>
+        /// An irrational decimal fraction is created by concatenating the positive integers:
+        /// 
+        /// 0.123456789101112131415161718192021...
+        /// 
+        /// It can be seen that the 12th digit of the fractional part is 1.
+        /// 
+        /// If d_n represents the nth digit of the fractional part, find the value of the following expression.
+        /// 
+        /// d_1 × d_10 × d_100 × d_1000 × d_10000 × d_100000 × d_1000000
+        /// </summary>
+        private static void Problem_040()
+        {
+            var sb = new StringBuilder(1000100);
+
+            for (var i = 1; sb.Length < 1000000; i++)
+            {
+                sb.Append(i);
+            }
+
+            Func<int, int> d = index =>
+            {
+                return (int)(sb[index - 1] - '0');
+            };
+
+            var product = d(1) * d(10) * d(100) * d(1000) * d(10000) * d(100000) * d(1000000);
+            Console.WriteLine("product = " + product);
         }
 
         /// <summary>
