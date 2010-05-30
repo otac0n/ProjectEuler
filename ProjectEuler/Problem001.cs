dc079fdf718687594b16893649c8fc457a438a25
John Gietzen <otac0n@gietzen.us>
2010-05-30 02:57:59 +0000

    Converted Problem 1 to a class.
    git-svn-id: http://otac0n.com:3000/projects/ProjectEuler/trunk@1479 14c0b8bc-c9cf-8f41-ba33-e04da095c157

---
+++ ProjectEuler/Problem001.cs
@@ -0,0 +1,29 @@
+﻿namespace ProjectEuler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+    ///
+    /// Find the sum of all the multiples of 3 or 5 below 1000.
+    /// </summary>
+    public class Problem001 : Problem
+    {
+        public override void Solve(string resource)
+        {
+            var sum = 0;
+
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine("sum = " + sum);
+        }
+    }
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -16,7 +16,7 @@ namespace ProjectEuler
     {
         private static void Main(string[] args)
         {
-            var loader = new ProblemLoader<Problem000>();
+            var loader = new ProblemLoader<Problem001>();
             var resource = loader.LoadResource();
 
             var sw = new Stopwatch();
@@ -30,27 +30,6 @@ namespace ProjectEuler
             Console.WriteLine();
             Console.WriteLine("Solution Found in " + sw.Elapsed + ".");
             Console.ReadKey(true);
-        }
-
-        /// <summary>
-        /// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-        ///
-        /// Find the sum of all the multiples of 3 or 5 below 1000.
-        /// </summary>
-        private static void Problem_001()
-        {
-            var numbers = new List<int>();
-
-            for (int i = 1; i < 1000; i++)
-            {
-                if (i % 3 == 0 || i % 5 == 0)
-                {
-                    numbers.Add(i);
-                    continue;
-                }
-            }
-
-            Console.WriteLine("sum = " + numbers.Sum());
         }
 
         /// <summary>
