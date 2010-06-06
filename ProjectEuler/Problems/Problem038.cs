44c3c35fbf6b7a7f42c8064e73c3581b52828827
John Gietzen <otac0n@gietzen.us>
2010-06-06 07:22:03 +0000

    Added solution to Problem 38.
    git-svn-id: http://otac0n.com:3000/projects/ProjectEuler/trunk@1502 14c0b8bc-c9cf-8f41-ba33-e04da095c157

--- ProjectEuler/Problems/Problem038.cs
+++ ProjectEuler/Problems/Problem038.cs
@@ -17,12 +17,66 @@
     /// 
     /// What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2, ... , n) where n > 1?
     /// </summary>
-    [Result(Name = "largest", Expected = "")]
+    [Result(Name = "largest", Expected = "932718654")]
     public class Problem038: Problem
     {
         public override string Solve(string resource)
         {
-            return "";
+            var largest = 0;
+            var largestI = 0;
+            for (int i = 1; i <= 9876; i++)
+            {
+                var digits = new short[10];
+                var concat = string.Empty;
+                for (int n = 1; n <= 9; n++)
+                {
+                    var v = (i * n).ToString();
+                    foreach (var c in v)
+                    {
+                        digits[(int)(c - '0')]++;
+                    }
+
+                    if (digits[0] != 0)
+                    {
+                        break;
+                    }
+
+                    if (digits[1] > 1 ||
+                        digits[2] > 1 ||
+                        digits[3] > 1 ||
+                        digits[4] > 1 ||
+                        digits[5] > 1 ||
+                        digits[6] > 1 ||
+                        digits[7] > 1 ||
+                        digits[8] > 1 ||
+                        digits[9] > 1)
+                    {
+                        break;
+                    }
+
+                    concat += v;
+
+                    if (digits[1] == 1 &&
+                        digits[2] == 1 &&
+                        digits[3] == 1 &&
+                        digits[4] == 1 &&
+                        digits[5] == 1 &&
+                        digits[6] == 1 &&
+                        digits[7] == 1 &&
+                        digits[8] == 1 &&
+                        digits[9] == 1)
+                    {
+                        if (n > 1)
+                        {
+                            largest = Math.Max(largest, int.Parse(concat));
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            return largest.ToString();
         }
     }
 }
