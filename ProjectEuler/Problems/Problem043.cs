c61b0b057501b666dcaf64d68c5025ac7ca3d564
John Gietzen <otac0n@gietzen.us>
2010-06-06 07:38:27 +0000

    Added solution to Problem 43.
    git-svn-id: http://otac0n.com:3000/projects/ProjectEuler/trunk@1503 14c0b8bc-c9cf-8f41-ba33-e04da095c157

--- ProjectEuler/Problems/Problem043.cs
+++ ProjectEuler/Problems/Problem043.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Combinatorics;
 
     /// <summary>
     /// The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits 0 to 9 in some order, but it also has a rather interesting sub-string divisibility property.
@@ -19,12 +20,42 @@
     /// 
     /// Find the sum of all 0 to 9 pandigital numbers with this property.
     /// </summary>
-    [Result(Name = "sum", Expected = "")]
+    [Result(Name = "sum", Expected = "16695334890")]
     public class Problem043 : Problem
     {
         public override string Solve(string resource)
         {
-            return "";
+            long sum = 0;
+            foreach(IList<int> digits in new Permutations<int>(new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, GenerateOption.WithoutRepetition))
+            {
+                if (digits[0] == 0)
+                {
+                    continue;
+                }
+
+                if (((digits[1] * 100 + digits[2] * 10 + digits[3] * 1) % 02 == 0) &&
+                    ((digits[2] * 100 + digits[3] * 10 + digits[4] * 1) % 03 == 0) &&
+                    ((digits[3] * 100 + digits[4] * 10 + digits[5] * 1) % 05 == 0) &&
+                    ((digits[4] * 100 + digits[5] * 10 + digits[6] * 1) % 07 == 0) &&
+                    ((digits[5] * 100 + digits[6] * 10 + digits[7] * 1) % 11 == 0) &&
+                    ((digits[6] * 100 + digits[7] * 10 + digits[8] * 1) % 13 == 0) &&
+                    ((digits[7] * 100 + digits[8] * 10 + digits[9] * 1) % 17 == 0))
+                {
+                    sum +=
+                        digits[9] * 1L +
+                        digits[8] * 10L +
+                        digits[7] * 100L +
+                        digits[6] * 1000L +
+                        digits[5] * 10000L +
+                        digits[4] * 100000L +
+                        digits[3] * 1000000L +
+                        digits[2] * 10000000L +
+                        digits[1] * 100000000L +
+                        digits[0] * 1000000000L;
+                }
+            }
+
+            return sum.ToString();
         }
     }
 }
